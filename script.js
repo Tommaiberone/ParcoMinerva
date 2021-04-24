2f7e7822dddf2700f17fd2f1fc33bcb9e8e2bdbc
Tommaiberone <tommasobersani@gmail.com>
2021-04-24 18:31:08 +0200

    Animazione attrazioni + aggiunta bottom bar + miglioramenti vari

--- script.js
+++
@@ -1,4 +0,0 @@
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.documentElement.scrollTop = 1010101000; // For Chrome, Firefox, IE and Opera
-}
