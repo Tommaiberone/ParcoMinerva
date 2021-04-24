ece4b18d094f4a0eeab99baff6e215d96dd85d16
Tommaiberone <tommasobersani@gmail.com>
2021-04-24 13:26:36 +0200

    Modifiche varie alla home page

---
+++ script.js
@@ -0,0 +1,4 @@
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 1010101000; // For Chrome, Firefox, IE and Opera
+}
