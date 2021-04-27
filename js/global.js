724dc68ee296f06c480c5ec62ed91c109a8b797a
Tommaiberone <tommasobersani@gmail.com>
2021-04-27 19:36:32 +0200

    Bug Fix

--- js/global.js
+++ js/global.js
@@ -1,42 +1,33 @@
-var barra = document.getElementById("navibar");
-
-
 //Controls the fade-in/fade-out of the bar
-function hideIF() {
-    var prevScrollpos = window.pageYOffset;
-    var hoverSpace = $('body');
+var barra = document.getElementById("navibar");
+var prevScrollpos = window.pageYOffset;
+var hoverSpace = $('body');
 
-    window.onscroll = function() {
+window.onscroll = function() {
         
-        var currentScrollPos = window.pageYOffset;
+    var currentScrollPos = window.pageYOffset;
         
-        //Hides the bar if the cursor is under a certain threshold 
-
-        if (currentScrollPos > 100) {
-
-            hoverSpace.on('mousemove', function(event) {
+    //Hides the bar if the cursor is under a certain threshold 
+    hoverSpace.on('mousemove', function(event) {
             
-                if(event.clientY < 150) {
-                    barra.style.top = "0";
-                } else {
-                    barra.style.top = "-300px";
-                }
-            });
-        }
-        
-        //Hides the bar if the cuser scrolls down the page
-
-        if (prevScrollpos > currentScrollPos) {
+        if(event.clientY < 150) {
             barra.style.top = "0";
-        } else if(currentScrollPos > 100){
+        } else if (currentScrollPos > 100){
             barra.style.top = "-300px";
         }
-        prevScrollpos = currentScrollPos;
-
+    });
+        
+    //Hides the bar if the cuser scrolls down the page
+    if (prevScrollpos > currentScrollPos) {
+        barra.style.top = "0";
+    } else if(currentScrollPos > 100){
+        barra.style.top = "-300px";
     }
+    prevScrollpos = currentScrollPos;
+
+}
 
     
-}
 
 
 
