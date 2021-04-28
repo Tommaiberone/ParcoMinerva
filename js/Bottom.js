8e093f11d32393fcce2cf55b40e7d7107f36f61b
Tommaiberone <tommasobersani@gmail.com>
2021-04-28 13:01:28 +0200

    Miglioramenti vari scheda attrazioni + Riordinamento css

--- js/Bottom.js
+++ js/Bottom.js
@@ -1,7 +1,9 @@
 
-var myvar = '<div id="bottom_bar" class="navbar">'+
-'            <button id="scrollButton" onclick="window.scroll(0,0);" class="btn scopri" >Torna all\'inizio</button>'+
+var myvar = '<br><br><br><br><div id="bottom_bar" class="navbar">'+
+'            <button id="backToTopButton" onclick="window.scroll(0,0);" class="btn scopri" >Torna all\'inizio</button>'+
 '            <span id="Copyright" > <i>©2021, Aloisi-Bersani, all rights reserved </i>  </span>'+
-'        </div>';
+'        </div>'+
+// Closes the wrapper div in the top_bar
+'    </div>';
 	
 document.write(myvar);
--- js/Top_bar.js
+++ js/Top_bar.js
@@ -31,45 +31,48 @@ if (dir == "/biglietti") {
 
 
 var contatti = ''+
-'        <div id = "HoverSpace" class = "container-fluid sticky-top">'+
-'            <br>'+
-'            <nav id = "navibar" class = "navbar navbar-expand-sm navbar-light rounded-pill border border-primary border-2 shadow ms-0 me-0 " style = "background-color: #e3f2fd">'+
-''+
-'                <!-- Logo -->'+
-'                <a class = "navbar-brand ms-3" href = "../index/index.html">'+
-'                    <img src = "../images/logo-sapienza-new.jpg" style = "width: 120px; height: 40px; margin-top: -10%; margin-bottom: -7%;" class = "rounded rounded-pill">    '+
-'                </a>'+
-''+
-'                <!-- Links Home - Attrazioni - Contatti - Biglietti -->'+
-'                <ul class = "navbar-nav ms-auto">'+
-'                    <li class = "nav-item">'+
-'                        <a class = "nav-link ' + HomeActive + ' " href = "' + preHome + 'index.html"> <b> Home </b> </a>'+
-'                    </li>'+
-''+
-'                    <li class = "nav-item">'+
-'                        <a class = "nav-link ' + AttrazioniActive + ' " href = "' + preAttrazioni + 'attractions.html"> <b> Attrazioni </b> </a>'+
-'                    </li>'+
-''+
-'                    <li class = "nav-item">'+
-'                        <a class = "nav-link ' + ContattiActive + ' " href = "' + preContatti + 'contatti.html"> <b> Contatti </b> </a>'+
-'                    </li>'+
-''+
-'                    <li class = "nav-item">'+
-'                        <a class = "nav-link ' + BigliettiActive + ' " href = "' + preBiglietti + 'biglietti.html"> <b> Biglietti </b> </a>'+
-'                    </li>'+
-'                </ul>'+
-''+
-'                <!-- Link Login/SignUp -->'+
-'                <ul class = "navbar-nav ms-auto me-3">'+
-'                    <li class = "nav-item">'+
-'                        <a id = "signup" type = "button" class = "btn btn-primary rounded me-2" href = "../login-register/register.html"> <b> Registrati </b> </a>'+
-'                    </li>'+
-''+
-'                    <li class = "nav-item">'+
-'                        <a id = "login" type = "button" class = "btn btn-primary rounded" href = "../login-register/login.html"> <b> Accedi </b> </a>'+
-'                    </li>'+
-'                </ul>'+
-'            </nav>'+
-'        </div>';
+'        <div id = "Wrapper">'+ //Opens a div that wraps all the content in the page (it's meant to
+                                //have the bottom bar sticking to the bottom of the page even if the
+                                //page content does not cover the entire screen)
+    '        <div id = "HoverSpace" class = "container-fluid sticky-top">'+
+    '            <br>'+
+    '            <nav id = "navibar" class = "navbar navbar-expand-sm navbar-light rounded-pill border border-primary border-2 shadow ms-0 me-0 " style = "background-color: #e3f2fd">'+
+    ''+
+    '                <!-- Logo -->'+
+    '                <a class = "navbar-brand ms-3" href = "../index/index.html">'+
+    '                    <img src = "../images/logo-sapienza-new.jpg" style = "width: 120px; height: 40px; margin-top: -10%; margin-bottom: -7%;" class = "rounded rounded-pill">    '+
+    '                </a>'+
+    ''+
+    '                <!-- Links Home - Attrazioni - Contatti - Biglietti -->'+
+    '                <ul class = "navbar-nav ms-auto">'+
+    '                    <li class = "nav-item">'+
+    '                        <a class = "nav-link ' + HomeActive + ' " href = "' + preHome + 'index.html"> <b> Home </b> </a>'+
+    '                    </li>'+
+    ''+
+    '                    <li class = "nav-item">'+
+    '                        <a class = "nav-link ' + AttrazioniActive + ' " href = "' + preAttrazioni + 'attractions.html"> <b> Attrazioni </b> </a>'+
+    '                    </li>'+
+    ''+
+    '                    <li class = "nav-item">'+
+    '                        <a class = "nav-link ' + ContattiActive + ' " href = "' + preContatti + 'contatti.html"> <b> Contatti </b> </a>'+
+    '                    </li>'+
+    ''+
+    '                    <li class = "nav-item">'+
+    '                        <a class = "nav-link ' + BigliettiActive + ' " href = "' + preBiglietti + 'biglietti.html"> <b> Biglietti </b> </a>'+
+    '                    </li>'+
+    '                </ul>'+
+    ''+
+    '                <!-- Link Login/SignUp -->'+
+    '                <ul class = "navbar-nav ms-auto me-3">'+
+    '                    <li class = "nav-item">'+
+    '                        <a id = "signup" type = "button" class = "btn btn-primary rounded me-2" href = "../login-register/register.html"> <b> Registrati </b> </a>'+
+    '                    </li>'+
+    ''+
+    '                    <li class = "nav-item">'+
+    '                        <a id = "login" type = "button" class = "btn btn-primary rounded" href = "../login-register/login.html"> <b> Accedi </b> </a>'+
+    '                    </li>'+
+    '                </ul>'+
+    '            </nav>'+
+    '        </div>';
 	
 document.write(contatti);
