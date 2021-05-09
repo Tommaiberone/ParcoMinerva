a886c0d455146edee84223118ca62671fd28d677
aloilor <aloisi.lorenzo99@gmail.com>
2021-05-09 22:21:22 +0200

    random commit

--- login-register/bentornato.php
+++ login-register/bentornato.php
@@ -5,7 +5,7 @@
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>PDM - Registrazione</title>
+        <title>PDM - Accedi</title>
 
         <link rel="stylesheet" href="../css/bootstrap.css">
         <link rel = "stylesheet" type = "text/css" href = "../css/global.css">
