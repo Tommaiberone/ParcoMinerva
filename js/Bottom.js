c52d7c2bf131107de41ed605f34b2bdf1bfecc6b
Tommaiberone <tommasobersani@gmail.com>
2021-05-24 11:34:14 +0200

    Responsiveness a bestia

--- js/Bottom.js
+++ js/Bottom.js
@@ -1,9 +1,10 @@
 
-var myvar = '<br><br><br><br><div id="bottom_bar" class="navbar">'+
-'            <button id="backToTopButton" onclick="window.scroll(0,0);" class="btn scopri" >Torna all\'inizio</button>'+
-'            <span id="Copyright" > <i>©2021, Aloisi-Bersani, all rights reserved </i>  </span>'+
-'        </div>'+
+var myvar = '<br><br><br><br>'+
+'            <div id="bottom_bar" class="navbar">'+
+'               <button id="backToTopButton" onclick="window.scroll(0,0);" class="btn scopri" >Torna all\'inizio</button>'+
+'               <span id="Copyright" > <i>©2021, Aloisi-Bersani, all rights reserved </i>  </span>'+
+'            </div>'+
 // Closes the wrapper div in the top_bar
-'    </div>';
+'         </div>';
 	
 document.write(myvar);
